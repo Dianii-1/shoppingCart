0b96715b0a148b187a3ea3016ef35bf48ab9a700
Diana Pinilla <dianii230416@gmail.com>
2025-05-27 11:03:22 -0500

    refactor: async page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,13 +1,14 @@
 import { Sidebar, TopMenu } from "@/components";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
   return (
     <>
-      <Sidebar />
+      <Sidebar/>
       <div className="ml-auto mb-6 bg-gray-200 lg:w-[75%] xl:w-[80%] 2xl:w-[85%] min-h-screen">
         <TopMenu />
         <div className="px-6 pt-6 bg-white p-2 pb-6 m-2 rounded-xl">{children}</div>
