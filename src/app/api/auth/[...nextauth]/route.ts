dff88a241083992ab8891e5dac24bae04aab51cf
Diana Pinilla <dianii230416@gmail.com>
2025-05-27 11:40:50 -0500

    feat: Add adapter in next auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from "@/lib/prisma"
 
 
 // https://next-auth.js.org/getting-started/example ruta para realizar la autenticacion con nextAuth
@@ -12,8 +14,9 @@ import GoogleProvider from "next-auth/providers/google"
 // https://console.cloud.google.com/apis/credentials?inv=1&invt=AbyGZw&project=admin-todos-460619&supportedpurview=project obtener credenciales
 // se crea un nuevo proyecto/ se da en crear credenciales/ crear id de cliente oauth/ configurar pantalla de concentimiento
 
-
 export const authOptions = {
+  // cinfigure adapter
+  adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
