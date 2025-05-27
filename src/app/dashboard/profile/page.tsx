cc058e6f2c68df4e9861cfd84cb79fd1dfc8f642
Diana Pinilla <dianii230416@gmail.com>
2025-05-27 11:02:59 -0500

    feat: Create profilepage

---
+++ src/app/dashboard/profile/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { useSession } from "next-auth/react";
+
+export default function ProfilePage() {
+
+    // este solo se utiliza del lado del cliente para obtener la sesion autenticada
+    // pero para utilizarlo tiene que estar dentro de un provider y este tiene que estar en la parte mas alta
+    // de la aplicacion osea ene l layout
+    const {data:session} = useSession()
+  return (
+    <div className="flex flex-col gap-4">
+      <h1>Perfil del lado del cliente</h1>
+      <hr />
+      <span>{session?.user?.name}</span>
+      <span>{session?.user?.email}</span>
+      <span>{session?.user?.image}</span>
+    </div>
+  );
+}
