f0f7f16286b44b1e9468013dfb7888c8938c19bb
Diana Pinilla <dianii230416@gmail.com>
2025-05-27 10:59:48 -0500

    feat: Add new patterns

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,8 @@ const nextConfig: NextConfig = {
     remotePatterns:[
       {protocol:'https', hostname:'encrypted-tbn0.gstatic.com'},
       {protocol:'https', hostname:'images.unsplash.com'},
+      {protocol:'https', hostname:'avatars.githubusercontent.com'},
+      {protocol:'https', hostname:'lh3.googleusercontent.com'}
     ]
   }
 };
