8733784104edc217d3e32f8e34d9c78e0256a04b
Diana Pinilla <dianii230416@gmail.com>
2025-05-27 13:44:37 -0500

    feat: Add new data in session user

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
@@ -14,7 +14,7 @@ import prisma from "@/lib/prisma"
 // https://console.cloud.google.com/apis/credentials?inv=1&invt=AbyGZw&project=admin-todos-460619&supportedpurview=project obtener credenciales
 // se crea un nuevo proyecto/ se da en crear credenciales/ crear id de cliente oauth/ configurar pantalla de concentimiento
 
-export const authOptions = {
+export const authOptions:NextAuthOptions = {
   // cinfigure adapter
   adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
@@ -29,6 +29,55 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+
+  // se agrega esto para configurar los datos que se retornan y poder obteenr los que yo cree
+
+  session:{
+    strategy:'jwt'
+  },
+
+  // los callbacks es por donde pasa despues de realizar la autenticacion por los providers
+  // estos son los valores por defecto que se tienen
+  // async signIn (){
+  //     return true
+  //   },
+
+  //   async jwt ({token}) {
+  //     return token
+  //   },
+
+  //   async session({session}){
+  //     return session
+  //   }
+
+  callbacks:{
+    async signIn (){
+      return true;
+    },
+
+    async jwt ({token}) {
+      // console.log(token)
+      // esto se realiza para encontrar el usuario en la base de datos para agregar en el token los roles y el id
+      const dbUser = await prisma.user.findUnique({where:{email: token.email ?? 'no-email'}});
+
+      token.roles = dbUser?.roles ?? ["no-roles"];
+      token.id = dbUser?.id ?? "no-uuid";
+
+      return token;
+    },
+
+    async session({session, token}){
+
+      // aca se agregan los campos faltantes para poder mostrarlos en el front
+      // ya no muestra error de tipado ya que se agrego el archivo nextauth.d.ts
+      if(session && session.user) {
+        session.user.roles = token.roles;
+        session.user.id = token.id;
+      }
+
+      return session
+    }
+  }
 }
 
 const handler = NextAuth(authOptions)
