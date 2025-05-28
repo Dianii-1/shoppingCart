519e4125ae35b9216ee1c337f4b448034c135ba3
Diana Pinilla <dianii230416@gmail.com>
2025-05-28 09:46:19 -0500

    refactor: Change api seed

--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -1,18 +1,38 @@
 import prisma from "@/lib/prisma";
+import bcrypt from "bcryptjs";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   await prisma.todo.deleteMany(); // es igual que hacer DELETE * FROM todo
+  await prisma.user.deleteMany(); // se elimina todos los usuarios ya creados
 
-  await prisma.todo.createMany({
-    data: [
-      { description: "Piedra del alma", complete: true },
-      { description: "Piedra del poder" },
-      { description: "Piedra del tiempo" },
-      { description: "Piedra del espacio" },
-      { description: "Piedra del realidad" },
-    ],
+   await prisma.user.create({
+    data: {
+      email: "usuario.prueba@hotmail.com",
+      password: bcrypt.hashSync("123456"),
+      roles: ["user", "admin"],
+      todos: {
+        create: [
+          { description: "Piedra del alma", complete: true },
+          { description: "Piedra del poder" },
+          { description: "Piedra del tiempo" },
+          { description: "Piedra del espacio" },
+          { description: "Piedra del realidad" },
+        ],
+      },
+    },
   });
+
+  // se comenta ya que se creo la relacion de user con todos hace falta asignarle un usuario
+  // await prisma.todo.createMany({
+  //   data: [
+  //     { description: "Piedra del alma", complete: true },
+  //     { description: "Piedra del poder" },
+  //     { description: "Piedra del tiempo" },
+  //     { description: "Piedra del espacio" },
+  //     { description: "Piedra del realidad" },
+  //   ],
+  // });
 
   return NextResponse.json({
     message: "Seed executed",
