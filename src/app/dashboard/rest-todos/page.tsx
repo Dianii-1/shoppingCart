690fa0ad851a3750b0daf9d91b5aec061714f85d
Diana Pinilla <dianii230416@gmail.com>
2025-05-28 10:25:50 -0500

    refactor: Get todos by user

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -4,8 +4,10 @@
 export const dynamic = 'force-dynamic'
 export const revalidate = 0
 
+import { getUserSessionServer } from "@/auth/action/auth-actions";
 import prisma from "@/lib/prisma";
 import { NewTodo, TodosGrid } from "@/todos";
+import { redirect } from "next/navigation";
 
 export const metadata = {
  title: 'Listado de TODOS',
@@ -14,7 +16,14 @@ export const metadata = {
 
 export default async function RestTodosPage() {
 
-  const todos = await prisma.todo.findMany({orderBy:{description:'asc'}})
+   const user = await getUserSessionServer();
+
+  if(!user) redirect('/api/auth/signin')
+
+  const todos = await prisma.todo.findMany({
+    where:{userId:user.id},
+    orderBy:{description:'asc'}
+  })
 
   return (
     <div>
--- src/app/dashboard/server-todos/page.tsx
+++ src/app/dashboard/server-todos/page.tsx
@@ -1,5 +1,7 @@
+import { getUserSessionServer } from "@/auth/action/auth-actions";
 import prisma from "@/lib/prisma";
 import { NewTodo, TodosGrid } from "@/todos";
+import { redirect } from "next/navigation";
 
 export const metadata = {
  title: 'Listado de TODOS',
@@ -8,7 +10,14 @@ export const metadata = {
 
 export default async function ServerTodosPage() {
 
-  const todos = await prisma.todo.findMany({orderBy:{description:'asc'}})
+  const user = await getUserSessionServer();
+
+  if(!user) redirect('/api/auth/signin')
+
+  const todos = await prisma.todo.findMany({
+    where:{userId:user.id},
+    orderBy:{description:'asc'}
+  })
 
   return (
     <div>
